b3fc5cc894f4ea297bc955e53b952e1ca1187ac8
Richard Bangs <bangs@bossalien.com>
2014-01-25 18:00:10 +0000

    Microphone input now works, wool!

--- UnityProject/Assets/Logic/MicrophoneInput.cs
+++ UnityProject/Assets/Logic/MicrophoneInput.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 public class MicrophoneInput
 {
 	AudioSource audioSource;
 	string deviceName;
+
+	//	Use me if we have to... Hardcoding a noise for now.
+	Queue<float> movingAverageBackgroundNoise = new Queue<float>();
+
+	const float noiseLevel = 0.5f;
+	const float lockOutTime = 0.5f;
+
+	float timeSinceLastLockOut = 0.0f;
+
+	public static event System.Action OnHammer = delegate {};
 
 	public MicrophoneInput()
 	{
@@ -21,36 +32,51 @@ public class MicrophoneInput
 		deviceName = Microphone.devices[0];
 		Debug.Log( "Using Microphone Device: " + deviceName );
 
-		audioSource.clip = Microphone.Start(deviceName, true, 3, 44100);
+		audioSource.clip = Microphone.Start(deviceName, true, 60*60, 44100);
+		audioSource.Play();
 	}
-
-	float timer = 3.0f;
 
 	public void UpdateTest()
 	{
 		if( string.IsNullOrEmpty( deviceName ) )
 			return;
 
-		timer -= Time.deltaTime;
+		timeSinceLastLockOut -= Time.deltaTime;
 
-		if( timer > 0.0f )
+		if( timeSinceLastLockOut > 0.0f )
 			return;
 
-		Debug.Log ( "Playing" );
-
-		timer = 5.0f;
-
-		Microphone.End( deviceName );
-
-		audioSource.Play();
+		TestForBang();
 	}
 
 	public void TestForBang()
 	{
-		var samples = new float[audioSource.clip.samples * audioSource.clip.channels];
+		const int numSamples = 1024;
+		var samples = new float[numSamples];
 
-		//AudioSource
+		samples = audioSource.GetOutputData( numSamples, 0 );
 
-		//for( int nIndex = 0; nIndex < samples.
+		float maxSample = -1.0f;
+		float sumOfSamples = 0.0f;
+		float x = 0.0f;
+		foreach( var sample in samples )
+		{		
+			x += 0.001f;
+
+			var start = new Vector3( x, 0.0f, 0.0f );
+			var end = new Vector3( x, sample, 0.0f );
+			Debug.DrawLine( start, end );
+			
+			if( sample > maxSample )
+				maxSample = sample;
+		}
+
+		if( maxSample > noiseLevel )
+		{
+			timeSinceLastLockOut = lockOutTime;
+			Debug.Log ( "ORDER!!!" );
+
+			OnHammer();
+		}
 	}
 }
